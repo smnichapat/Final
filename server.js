bf07b2ab1c60c353109911cbab23f997d6126f3a
smnichapat <57453788+smnichapat@users.noreply.github.com>
2019-12-03 17:39:09 +0700

    ans#4

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // #3 Serve static content in folder frontend
-
+app.use(express.static("frontend"));
 // ===============================
 
 
